dedb388a499d7c6683efc61638dcd79c4d3ce455
jaromero3rd <jaromero3rd@gmail.com>
2023-02-03 22:01:41 -0500

    server

--- server/server.js
+++ server/server.js
@@ -40,12 +40,11 @@ app.post('/', async (req, res) => {
       temperature: 0.6, // Higher values means the model will take more risks.
       max_tokens: 100, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      n: 1, //controls the number of sub-completions generated
+      //n: 1, //controls the number of sub-completions generated
       frequency_penalty: 2, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
       presence_penalty: 1, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
       stop: "END",
       });
-      console.log(isValidInput(`${prompt}`))
       res.status(200).send({
         bot: response.data.choices[0].text
       });
