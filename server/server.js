17caae8ba3513cd7a69cb678fa02117bce1fea55
jaromero3rd <jaromero3rd@gmail.com>
2023-02-03 22:45:14 -0500

    server

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.post('/', async (req, res) => {
       const response = await openai.createCompletion({
       model: "text-davinci-001",
       prompt: isValidInput(`${prompt}`), //`${prompt}`,
-      temperature: 0.6, // Higher values means the model will take more risks.
+      temperature: 0.9, // Higher values means the model will take more risks.
       max_tokens: 100, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
       //n: 1, //controls the number of sub-completions generated
