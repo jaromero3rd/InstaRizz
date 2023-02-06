e96bab1afc894a4c038877cbbcc366ee2e2de510
jaromero3rd <jaromero3rd@gmail.com>
2023-02-06 07:27:03 -0500

    promp

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ app.post('/', async (req, res) => {
       model: "davinci:ft-finhelp-2023-02-03-00-07-40",
       prompt: isValidInput(`${prompt}`), //`${prompt}`,
       temperature: 0.7, // Higher values means the model will take more risks.
-      max_tokens: 256, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      max_tokens: 150, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
       //n: 1, //controls the number of sub-completions generated
       frequency_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
